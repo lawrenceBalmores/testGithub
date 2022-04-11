308ada057fdb896d8b0d15b5be0b81c937e750a3
Lawrence <lawbalmores@gmail.com>
2022-04-11 15:04:04 +0800

    my second commit

--- Github/src/java_test_two.java
+++ Github/src/java_test_two.java
@@ -2,7 +2,7 @@
 public class java_test_two {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Congrats! You successfully pushed a block of code!");
 
 	}
 
